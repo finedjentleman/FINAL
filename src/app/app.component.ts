01784c09c25ee7217e21846127db2942d88fbdb7
finedjentleman <rawatabhi1729@gmail.com>
2019-05-29 13:02:45 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,22 +12,6 @@ export class AppComponent  {
   
   //PIE CHART!
   
-  //days: number[] = [21, 39, 10, 14];
-  
-  public pieChartLabels: string[] = ["Less than 5 days", "Less than 15 days", "Less than 30 days", "More than 30 days"];
-  public pieChartData: number[] = [21, 39, 10, 14];
-  public pieChartType: string = 'pie';
-  public pieChartOptions: any = {
-    'backgroundColor': [
-      "#FF6384",
-      "#4BC0C0",
-      "#FFCE56",
-      "#E7E9ED",
-      "#36A2EB",
-    ]
-  }
-  
-  
   // events on slice click
   public chartClicked(e:any):void {
     console.log(e);
@@ -39,52 +23,6 @@ export class AppComponent  {
   }
 
 //LINE CHART!
-
-  public SystemName: string = "No. of cases assigned";
-  firstCopy = false;
-  
-  //this will hold the number of cases assigned to each
-  public lineChartData: Array<number> = [ 1,8,49,50,51,15];
-
-  public labelMFL: Array<any> = [
-      { data: this.lineChartData,
-        label: this.SystemName
-      }
-  ];
-  // labels
-  //it should show the names of cases associated with each assignee
-  //so each should hold the array of test cases assigned to it
-  public lineChartLabels: Array<any> = ["2018-01-29 10:00:00", "2018-01-29 10:27:00", "2018-01-29 10:28:00", "2018-01-29 10:29:00", "2018-01-29 10:30:00" ];
-
-  constructor(  ) { }
-  
-  //store the maximum and assign to max
-  public lineChartOptions: any = {
-    responsive: true,
-    scales : {
-      yAxes: [{
-        ticks: {
-          max : 60,
-          min : 0,
-        }
-      }],
-      xAxes: [{
-          type: 'category',
-          labels:['A1', 'A2', 'A3', 'A4', 'A5', 'A6']
-        }],
-    },
-  };
-
-  _lineChartColors:Array<any> = [{
-       backgroundColor: 'red',
-        borderColor: 'red',
-        pointBackgroundColor: 'red',
-        pointBorderColor: 'red',
-        pointHoverBackgroundColor: 'red',
-        pointHoverBorderColor: 'red' 
-      }];
-
-  public lineChartType = 'line';  
 
   //SMART TABLE
     settings = {
--- src/app/tools-chart/tools-chart.component.ts
+++ src/app/tools-chart/tools-chart.component.ts
@@ -7,5 +7,50 @@ import { Component } from '@angular/core';
 })
 
 export class ToolsChartComponent{
+  public SystemName: string = "No. of cases assigned";
+  firstCopy = false;
+  
+  //this will hold the number of cases assigned to each
+  public lineChartData: Array<number> = [ 1,8,49,50,51,15];
+
+  public labelMFL: Array<any> = [
+      { data: this.lineChartData,
+        label: this.SystemName
+      }
+  ];
+  // labels
+  //it should show the names of cases associated with each assignee
+  //so each should hold the array of test cases assigned to it
+  public lineChartLabels: Array<any> = ["2018-01-29 10:00:00", "2018-01-29 10:27:00", "2018-01-29 10:28:00", "2018-01-29 10:29:00", "2018-01-29 10:30:00" ];
+
+  constructor(  ) { }
+  
+  //store the maximum and assign to max
+  public lineChartOptions: any = {
+    responsive: true,
+    scales : {
+      yAxes: [{
+        ticks: {
+          max : 60,
+          min : 0,
+        }
+      }],
+      xAxes: [{
+          type: 'category',
+          labels:['A1', 'A2', 'A3', 'A4', 'A5', 'A6']
+        }],
+    },
+  };
+
+  _lineChartColors:Array<any> = [{
+       backgroundColor: 'red',
+        borderColor: 'red',
+        pointBackgroundColor: 'red',
+        pointBorderColor: 'red',
+        pointHoverBackgroundColor: 'red',
+        pointHoverBorderColor: 'red' 
+      }];
+
+  public lineChartType = 'line';  
 
 }
