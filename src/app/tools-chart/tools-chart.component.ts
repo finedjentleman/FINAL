e1a8890371466d82724a2bdb74a4ea05c5320b2f
finedjentleman <rawatabhi1729@gmail.com>
2019-05-29 15:01:55 +0530

    7 files modified

--- src/app/tools-chart/tools-chart.component.ts
+++ src/app/tools-chart/tools-chart.component.ts
@@ -8,52 +8,30 @@ import { Component } from '@angular/core';
 
 export class ToolsChartComponent{
   
-
-  public lineChartType = 'line';  
-    
-  public SystemName: string = "No. of cases assigned";
-  firstCopy = false;
+  public pieChartLabels: string[] = ["A1", "A2", "A3", "A4", "A5", "A6"];
+  public pieChartData: number[] = [13, 10, 25, 20, 4, 19];
+  public pieChartType: string = 'pie';
+  public pieChartOptions: any = {
+    'backgroundColor': [
+      "#FF6384",
+      "#4BC0C0",
+      "#FFCE56",
+      "#E7E9ED",
+      "#36A2EB",
+    ]
+  }
   
-  //this will hold the number of cases assigned to each
-  public lineChartData: Array<number> = [ 1,8,49,50,51,15];
-
-  public labelMFL: Array<any> = [
-      { data: this.lineChartData,
-        label: this.SystemName
-      }
-  ];
-  // labels
-  //it should show the names of cases associated with each assignee
-  //so each should hold the array of test cases assigned to it
-  public lineChartLabels: Array<any> = ["2018-01-29 10:00:00", "2018-01-29 10:27:00", "2018-01-29 10:28:00", "2018-01-29 10:29:00", "2018-01-29 10:30:00" ];
-
-  constructor(  ) { }
   
-  //store the maximum and assign to max
-  public lineChartOptions: any = {
-    responsive: true,
-    scales : {
-      yAxes: [{
-        ticks: {
-          max : 60,
-          min : 0,
-        }
-      }],
-      xAxes: [{
-          type: 'category',
-          labels:['A1', 'A2', 'A3', 'A4', 'A5', 'A6']
-        }],
-    },
-  };
+  // events on slice click
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+ 
+  // event on pie chart slice hover
+  public chartHovered(e:any):void {
+    console.log(e);
+  }
 
-  _lineChartColors:Array<any> = [{
-       backgroundColor: 'red',
-        borderColor: 'red',
-        pointBackgroundColor: 'red',
-        pointBorderColor: 'red',
-        pointHoverBackgroundColor: 'red',
-        pointHoverBorderColor: 'red' 
-      }];
-
+  
 
 }
