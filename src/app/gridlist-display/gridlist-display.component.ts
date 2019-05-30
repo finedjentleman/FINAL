365d236179f615f65534d07dfdaf0e013096a7d8
finedjentleman <rawatabhi1729@gmail.com>
2019-05-31 00:42:54 +0530

    1 file modified

--- src/app/gridlist-display/gridlist-display.component.ts
+++ src/app/gridlist-display/gridlist-display.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'gridlist-display',
-  templateUrl: './gridlist-display.component.html',
-  styleUrls: ['./gridlist-display.component.css']
-})
 
 export interface Card{
   cols: number;
   rows: number;
   id: string;
 }
+
+@Component({
+  selector: 'gridlist-display',
+  templateUrl: './gridlist-display.component.html',
+  styleUrls: ['./gridlist-display.component.css']
+})
+
 
 export class GridlistDisplayComponent{
 
