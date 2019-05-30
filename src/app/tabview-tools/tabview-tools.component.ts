798112f5b20595967d6d1eb974e58fe79918ec71
finedjentleman <rawatabhi1729@gmail.com>
2019-05-30 16:36:20 +0530

    2 files modified

--- src/app/tabview-tools/tabview-tools.component.ts
+++ src/app/tabview-tools/tabview-tools.component.ts
@@ -1,2 +1,11 @@
 import { Component } from '@angular/core';
 
+@Component({
+  selector: 'tabview-tools',
+  templateUrl: 'tabview-tools.component.html',
+  styleUrls: ['tabview-tools.component.css']
+})
+
+export class TabviewToolsComponent{
+  
+}
